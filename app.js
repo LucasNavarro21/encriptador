b992ec7e4b8fbb7e6fe6525540700d26b7c5641d
LucasNava <navarrolucas4668@gmail.com>
2024-07-04 07:15:14 -0300

    Finalice la funcion de encriptar

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 
 let cadenaEncriptada = "";
-
+let cadenaDesencriptada = ""
 function obtenerTexto(){
     texto = document.querySelector(".inputEncriptar").value;
     return texto;
@@ -18,6 +18,12 @@ function procesoPalabra(){
     document.querySelector(".contenedorInputDesencriptar input").value = cadenaEncriptada;
 }
 
+function procesoPalabra2(){
+    cadena = obtenerTexto();
+    let cadenaDesencriptada = desconvertirLetra(cadena);
+    console.log(desconvertirLetra(cadena)); 
+    document.querySelector(".contenedorInputDesencriptar input").value = cadenaDesencriptada;
+}
 
 
 function convertirLetra(palabra){
@@ -39,9 +45,25 @@ function convertirLetra(palabra){
             palabra = "ufat"
             break;
     }
-    return palabra
+    return palabra;
 }
 
+function desconvertirLetra(palabra){
+
+    palabra = palabra.replace(/ai/g, "a")
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+        
+    return palabra;
+}
+
+function copiar(){
+   inputEncriptado = document.querySelector(".contenedorInputDesencriptar input").value;
+   document.querySelector(".contenedorInputEncriptar input").value = inputEncriptado; 
+
+}
 // cadena = texto.split(" ");
 // console.log(cadena);
 // console.log(convertirLetra("a")); 
@@ -49,15 +71,46 @@ function encriptado(cadena){
     cadenaEncriptada = ""; // Resetear la cadena encriptada antes de empezar
     for (let i = 0; i < cadena.length; i++) {
         const palabra = cadena[i];
-        let palabraEncriptada = ""; // Cadena temporal para la palabra encriptada
+        let palabraDesencriptada = ""; // Cadena temporal para la palabra encriptada
         for (let j = 0; j < palabra.length; j++) {
-            palabraEncriptada += convertirLetra(palabra[j]);
+            palabraDesencriptada += convertirLetra(palabra[j]);
         }
         // Añadir la palabra encriptada a la cadena final con un espacio
-        cadenaEncriptada += palabraEncriptada + " ";
+        cadenaEncriptada += palabraDesencriptada + " ";
     }
     // Eliminar el último espacio añadido
     return cadenaEncriptada.trim();
 }
+
+function desencriptado(cadena){
+    cadenaEncriptada = ""; // Resetear la cadena encriptada antes de empezar
+    for (let i = 0; i < cadena.length; i++) {
+        const palabra = cadena[i];
+        // console.log(palabra);
+        let palabraEncriptada = ""; // Cadena temporal para la palabra encriptada
+        for (let j = 0; j < palabra.length; j++) {
+            palabraEncriptada += desconvertirLetra(palabra[j]);
+        }
+        // Añadir la palabra encriptada a la cadena final con un espacio
+        cadenaEncriptada += palabraEncriptada + " ";
+        // console.log(palabraEncriptada);
+
+    }
+    // Eliminar el último espacio añadido
+    return cadenaEncriptada.trim();
+}
+
+// for (let i = 10; i > 0; i--) {
+//     // const element = array[i];
+//     console.log(i);    
+// }
+
+let saludo = "hola"
+for (let i = saludo.length - 1; i >= 0; i--) {
+
+        console.log(saludo[i]);
+    
+}
+
 
 
